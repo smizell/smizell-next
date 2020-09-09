7d9a0ace1ea7dddb8f7ed460f6c3da38f20dbb80
Stephen Mizell <smizell@gmail.com>
2020-09-08 20:35:49 -0500

    Move files around

--- components/navigation.js
+++ components/navigation.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <nav>
-      <Link href="/asc2020/">ASC 2020</Link>
-      <Link href="/asc2020/sections/introduction/">Introduction</Link>
-      <Link href="/asc2020/sections/use-cases/">Use Cases</Link>
-      <Link href="/asc2020/sections/closing-thoughts/">Closing Thoughts</Link>
+      <Link href="/">Home</Link>
+      <Link href="/sections/introduction/">Introduction</Link>
+      <Link href="/sections/use-cases/">Use Cases</Link>
+      <Link href="/sections/closing-thoughts/">Closing Thoughts</Link>
     </nav>
   );
 }
