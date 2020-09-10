1d6bf0953ef66ccfeb28e38f68a7a080a351ddf3
Stephen Mizell <smizell@gmail.com>
2020-09-09 22:37:24 -0500

    Add better meta data

--- layouts/index.js
+++ layouts/index.js
@@ -1,13 +1,31 @@
+import Head from "next/head";
 import Navigation from "@/components/navigation";
+import { Heading } from "@chakra-ui/core";
 
-export default function Index({ children }) {
+export default function Index({ children, frontMatter }) {
   return (
     <>
+      <Head>
+        <title>{frontMatter.title}</title>
+        <meta
+          name="description"
+          content="Presentation by Stephen Mizell at ASC2020 on a tool called Geneva"
+        />
+        <meta name="twitter:creator" content="@Stephen_Mizell" />
+        <meta name="og:url" content="https://present.smizell.com/" />
+        <meta name="og:title" content="Stephen Mizell" />
+        <meta
+          name="og:description"
+          content="Making OpenAPI and AsyncAPI Dynamic with Geneva"
+        />
+      </Head>
       <main>
         <Navigation />
+        <Heading as="h1" size="xl" my={4}>
+          {frontMatter.title}
+        </Heading>
         {children}
       </main>
-      ;
     </>
   );
 }
